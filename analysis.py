8f2a51e87dfaa197f984b68cdf83553cbd3e14d0
leojojo <setsunal.fujita@gmail.com>
2018-08-15 16:14:02 +0900

    single file analysis

--- analysis.py
+++ analysis.py
@@ -1,10 +1,6 @@
 # coding: utf-8
-import os,json
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+import sys,os,json
 
-path = b'./report/virusshare'
 total = {}
 
 # ========== VirusTotalのjsonはおかしいので修正 ==========
@@ -31,28 +27,19 @@ def scan_count(scans):
             else:
                 total[k] += 1
 
-# ========== グラフを描画 ==========
-def graph(a,b,c,d):
-    plt.rcParams['font.family'] = 'Yu Gothic'
-    labels = ['パック前データ1', 'パック後データ1', 'パック前データ2', 'パック後データ2']
-    height = [a, b, c, d]
-    fig, ax = plt.subplots(1,1)
-    ax.bar(labels, height, width=0.6)
-    plt.ylim([-100,0])
-    plt.ylabel('誤検知率(%)')
-    plt.title('正常ファイルの誤検知率')
-    plt.tight_layout()
-    plt.show()
-    plt.savefig('benign.png')
-
 # ========== ディレクトリの中全てのファイルについて ==========
-for dir in os.listdir(os.fsencode(path)):
-    for f in os.listdir(os.path.join(path,dir)):
-        fix_file(os.path.join(path,dir,f))
-        with open(os.path.join(path,dir,f), 'r') as f:
-            json_obj = json.load(f)
-            if "scans" in json_obj.keys():
-                scan_count(json_obj["scans"])
-            else:
-                continue
-print(total)
+def analyze(file):
+    fix_file(file)
+    with open(file, 'r') as f:
+        json_obj = json.load(f)
+        if "scans" in json_obj.keys():
+            scan_count(json_obj["scans"])
+    print(total)
+
+def main():
+    for arg in sys.argv[1:]:
+        print arg
+        analyze(arg)
+
+if __name__ == "__main__":
+    main()
