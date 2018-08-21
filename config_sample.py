e8d45062f12e9e8049231d1799c966e84fc3d3fb
leojojo <setsunal.fujita@gmail.com>
2018-08-22 03:58:35 +0900

    better practice virustotal.py

--- config_sample.py
+++ config_sample.py
@@ -1,2 +1,1 @@
 API_KEY = 'YOUR_API_KEY_HERE'
-path = b'./data/'
--- virustotal.py
+++ virustotal.py
@@ -1,57 +1,75 @@
-# coding: utf-8
-import os
-import requests
-import json
-import textwrap
-import config
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys,os,requests,json,textwrap
 from time import sleep
+from progressbar import ProgressBar
+try:
+    import config
+except ImportError:
+    print('cannot find config.py, see config_example.py for reference')
 
-request_ctr = 0
+class VirusTotalUploader:
+    def __init__(self):
+        self.request_ctr = 0
+        self.scan_id_list = []
+        self.base_url = 'https://www.virustotal.com/vtapi/v2/file/'
+        
+    # ========== ファイルを投げる ==========
+    def submit_file(self, path):
+        progress = ProgressBar(0, len(os.listdir(path)))
+        params = { 'apikey': config.API_KEY }
+        files = os.listdir(os.fsencode(path))
 
-# ========== ファイルを投げる ==========
-scan_id_list = []
-params = { 'apikey': config.API_KEY }
-directory = os.fsencode(config.path)
-for f in os.listdir(directory):
-    if f.startswith(b'.'):
-        continue
+        for index,f in enumerate(files):
+            if f.startswith(b'.'):
+                continue
 
-    if request_ctr >= 4:         # 4リクエスト / 分
-        sleep(60)
-        request_ctr = 0
+            if self.request_ctr >= 4:
+                sleep(60)
+                self.request_ctr = 0
 
-    filename = f.decode('utf-8')
-    files = { 'file': (filename, open(os.path.join(config.path,f), 'rb')) }
-    response = requests.post('https://www.virustotal.com/vtapi/v2/file/scan',
-            files=files, params=params)
-    request_ctr += 1
-    scan_id = str(response.json()['scan_id'])
-    scan_id_list.append(scan_id)
-    print(filename + ': ' + scan_id)
+            filename = f.decode('utf-8')
+            files = { 'file': (filename,
+                open(os.path.join(path,f.decode('utf-8')), 'rb')) }
+            response = requests.post(self.base_url + 'scan',
+                    files=files, params=params)
+            self.request_ctr += 1
+            scan_id = str(response.json()['scan_id'])
+            self.scan_id_list.append(scan_id)
 
-# ========== レポートを受け取る ==========
-os.makedirs('report', exist_ok=True)
+            progress.update(index)
 
-for index,scan_id in enumerate(scan_id_list):
-    print(str(request_ctr) + ': ' + str(scan_id))
+    # ========== レポートを受け取る ==========
+    def request_report(self,path):
+        os.makedirs('report', exist_ok=True)
 
-    if request_ctr >= 4:         # 4リクエスト / 分
-        sleep(60)
-        request_ctr = 0
+        for index,scan_id in enumerate(self.scan_id_list):
+            if self.request_ctr >= 4:         # 4リクエスト / 分
+                sleep(60)
+                self.request_ctr = 0
 
-    params = {
-        'apikey': config.API_KEY,
-        'resource': scan_id
-      }
-    headers = {
-        'Accept-Encoding': 'gzip, deflate',
-        'User-Agent' : 'python requests library'
-      }
-    response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',
-            params=params, headers=headers)
-    request_ctr += 1
-    result = str(response.json())
-    print(textwrap.shorten(result, width=100, placeholder="..."))
+            params = {
+                'apikey': config.API_KEY,
+                'resource': scan_id
+              }
+            headers = {
+                'Accept-Encoding': 'gzip, deflate',
+                'User-Agent' : 'python requests library'
+              }
+            response = requests.get(self.base_url + 'report',
+                    params=params, headers=headers)
+            self.request_ctr += 1
+            result = str(response.json())
 
-    with open(os.path.join('report', str(index)+'.json'), 'a') as f:
-        f.write(result)
+            with open(os.path.join('report', str(index)+'.json'), 'a') as f:
+                f.write(result)
+
+def main():
+    for arg in sys.argv[1:]:
+        v = VirusTotalUploader()
+        v.submit_file(arg)
+        v.request_report(arg)
+
+if __name__ == '__main__':
+    main()
